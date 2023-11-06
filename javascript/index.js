fc5be8556f560c7bc83be72385d3815cad952155
MahesaSyawalA <mahesaabd7@gmail.com>
2023-11-06 17:13:37 +0700

    add desc katakana and img for thanks section

--- javascript/index.js
+++ javascript/index.js
@@ -1,48 +1,61 @@
 const navbar = document.getElementById("navbar-container");
 const content = document.getElementById("content");
 
+const hiraganaButton = document.getElementById("hiraganaButton");
+const katakanaButton = document.getElementById("katakanaButton");
 const hiragana = document.getElementById("hiragana");
 const katakana = document.getElementById("katakana");
+
 let selection = false;
 
 function handleScroll() {
   if (window.scrollY > content.offsetTop) {
     navbar.style.background = "#A8E4EB";
-    localStorage.setItem('statusNavbar', 'solid')
+    localStorage.setItem("statusNavbar", "solid");
   } else {
     navbar.style.background = "transparent";
-    localStorage.setItem('statusNavbar', 'transparent')
+    localStorage.setItem("statusNavbar", "transparent");
   }
 }
 
-const initialNavbar = localStorage.getItem('statusNavbar');
-if(initialNavbar === 'solid'){
-    navbar.style.background = "#A8E4EB"; 
+const initialNavbar = localStorage.getItem("statusNavbar");
+if (initialNavbar === "solid") {
+  navbar.style.background = "#A8E4EB";
 }
 
 window.addEventListener("scroll", handleScroll);
 
 function handleHiragana() {
-  if(!selection){
-      selection = !selection;
-      hiragana.style.color= '#46C5D3';
-      hiragana.style.backgroundColor= 'white';
-      katakana.style.color= '';
-      katakana.style.backgroundColor= '';
-      console.log(selection)
-  } ;
+  if (!selection) {
+    selection = !selection;
+    hiraganaButton.style.color = "#46C5D3";
+    hiraganaButton.style.backgroundColor = "white";
+    katakanaButton.style.color = "";
+    katakanaButton.style.backgroundColor = "";
+    hiragana.style.display = "block";
+    katakana.style.display = "none";
+    console.log(selection);
+  }else{
+    hiragana.style.display = "block";
+    katakana.style.display = "none";
+  }
 }
 
 function handleKatakana() {
-    if(selection){
-        selection = !selection;
-        katakana.style.color= '#46C5D3';
-        katakana.style.backgroundColor= 'white';
-        hiragana.style.color= '';
-        hiragana.style.backgroundColor= '';
-        console.log(selection);
-    } ;
+  if (selection) {
+    selection = !selection;
+    katakanaButton.style.color = "#46C5D3";
+    katakanaButton.style.backgroundColor = "white";
+    hiraganaButton.style.color = "";
+    hiraganaButton.style.backgroundColor = "";
+    hiragana.style.display = "none";
+    katakana.style.display = "block";
+    console.log(selection);
+  }else{
+    hiragana.style.display = "none";
+    katakana.style.display = "block";
   }
+}
 
 // hiragana.addEventListener("click", function(){
 //     if(selection == true){
